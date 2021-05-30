77ddf30d25ed43002108262fb0050ef8eeb58b65
zORg <zorg-alex@ya.ru>
2021-05-30 18:49:59 +0300

    Light torture
    Fiddled with light and post to get somewhat decent indirect lighting result

---
+++ Assets/RotateSun.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[ExecuteInEditMode]
+public class RotateSun : MonoBehaviour
+{
+	public float time = 5f;
+	float timeStarted;
+	public float Angle = 120f;
+	public Vector3 Axis = Vector3.right;
+	Quaternion rotationAtStart;
+	Quaternion rotationAtEnd;
+    public InputAction action;
+	public Light Light;
+	public AnimationCurve lightIntensityOverDirection;
+	public float lightMaxIntensity;
+
+
+	private void Awake() {
+        action.Enable();
+		action.performed += Action_performed;
+		Axis = Axis.normalized;
+	}
+
+	[Button("Rotate")]
+	private void Action_performed(InputAction.CallbackContext e) {
+		timeStarted = Time.time;
+		rotationAtStart = transform.rotation;
+		rotationAtEnd = transform.rotation * Quaternion.AngleAxis(Angle, Axis);
+		StopAllCoroutines();
+		StartCoroutine(Rotate());
+	}
+
+	IEnumerator Rotate() {
+		while (transform.rotation != rotationAtEnd) {
+			yield return null;
+			transform.rotation = Quaternion.Lerp(rotationAtStart, rotationAtEnd, (Time.time - timeStarted) / time);
+			var dot = Vector3.Dot(Light.transform.forward, Vector3.down);
+			Light.intensity = lightIntensityOverDirection.Evaluate(dot) * lightMaxIntensity;
+		}
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.DrawLine(transform.position, transform.position + transform.TransformDirection(Axis));
+	}
+}
---
+++ Assets/TurnOffLights.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TurnOffLights : MonoBehaviour
+{
+    public InputAction action;
+    public List<Light> lights;
+    void Awake()
+    {
+        action.Enable();
+		action.performed += Action_performed;
+    }
+
+	private void Action_performed(InputAction.CallbackContext obj) {
+		foreach (var light in lights) {
+            light.enabled = !light.enabled;
+		}
+	}
+}
